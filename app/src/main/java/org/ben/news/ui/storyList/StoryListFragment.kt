6a1f393c9965e11b36724db2c9e160e7424b7a27
Ben Capper <Bencapper19@gmail.com>
2022-07-15 22:58:45 +0100

    Large Banner ad on Storylist frag
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/storyList/StoryListFragment.kt
+++ app/src/main/java/org/ben/news/ui/storyList/StoryListFragment.kt
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.snackbar.Snackbar
