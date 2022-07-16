4df3282f59e1c1e0b1d8eb3960c1cb9ec34a0292
Ben Capper <Bencapper19@gmail.com>
2022-07-16 13:48:12 +0100

    Breitbart ads
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/breitbart/BreitbartFragment.kt
+++ app/src/main/java/org/ben/news/ui/breitbart/BreitbartFragment.kt
@@ -12,6 +12,9 @@ import android.widget.ImageView
 import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.firebase.storage.FirebaseStorage
 import org.ben.news.R
 import org.ben.news.adapters.StoryAdapter
@@ -40,6 +43,8 @@ class BreitbartFragment : Fragment(), StoryListener {
     private val loggedInViewModel : LoggedInViewModel by activityViewModels()
     private val breitbartViewModel: BreitbartViewModel by activityViewModels()
     var state: Parcelable? = null
+    private lateinit var mAdView : AdView
+    lateinit var mAdViewTop : AdView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,6 +64,13 @@ class BreitbartFragment : Fragment(), StoryListener {
         fragBinding.recyclerViewBreit.layoutManager = activity?.let { LinearLayoutManager(it) }
         activity?.findViewById<ImageView>(R.id.toolimg)?.setImageResource(R.drawable.breit)
 
+        MobileAds.initialize(this.context!!) {}
+        mAdView = fragBinding.adViewBreitBot
+        mAdViewTop = fragBinding.adViewBreitTop
+        val adRequest = AdRequest.Builder().build()
+        val adRequestTop = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+        mAdViewTop.loadAd(adRequestTop)
 
         breitbartViewModel.observableBreitList.observe(viewLifecycleOwner) { story ->
             story?.let {
