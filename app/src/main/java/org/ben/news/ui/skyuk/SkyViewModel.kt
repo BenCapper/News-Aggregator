c18fefdb2ffeb26b23901b93160ecb95580c9906
Ben Capper <Bencapper19@gmail.com>
2022-07-25 20:13:21 +0100

    Sky Frag/vm/menu
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

---
+++ app/src/main/java/org/ben/news/ui/skyuk/SkyViewModel.kt
@@ -0,0 +1,86 @@
+package org.ben.news.ui.skyuk
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
+import org.ben.news.firebase.StoryManager
+import org.ben.news.models.StoryModel
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
+import kotlin.collections.ArrayList
+
+class SkyViewModel : ViewModel() {
+    private val skyList =
+        MutableLiveData<List<StoryModel>>()
+
+    val observableSkyList: LiveData<List<StoryModel>>
+        get() = skyList
+
+    private val story = MutableLiveData<StoryModel>()
+
+    var observableStory: LiveData<StoryModel>
+        get() = story
+        set(value) {story.value = value.value}
+
+    var liveFirebaseUser = MutableLiveData<FirebaseUser>()
+
+    //var readOnly = MutableLiveData(false)
+
+    init { load() }
+
+    private val outlet = "news.sky.com"
+    private val df = SimpleDateFormat("MM.dd.yy")
+    private val calDate = Calendar.getInstance().time
+    private var formattedDate: String = df.format(calDate)
+    private var today = formattedDate.replace(".","-")
+    private val now = LocalDate.now()
+
+    private fun getDates(n:Int): ArrayList<String> {
+        var dates = ArrayList<String>()
+        for (i in 0..n) {
+            val yesterday = now.minusDays(i.toLong())
+            val year = yesterday.year.toString().substring(2)
+            var month = yesterday.month.value.toString()
+            if (month.length == 1) {
+                month = "0$month"
+            }
+            var day = yesterday.dayOfMonth.toString()
+            if (day.length == 1) {
+                day = "0$day"
+            }
+            val date = "$month-$day-$year"
+            dates.add(date)
+        }
+
+        return dates
+    }
+
+
+    fun load() {
+        val list: ArrayList<String>
+        try {
+            list = getDates(5)
+            StoryManager.findByOutlet(list,outlet,skyList)
+            Timber.i("Load Success : ${skyList.value}")
+        }
+        catch (e: Exception) {
+            Timber.i("Load Error : $e.message")
+        }
+
+    }
+
+    fun search( term: String) {
+        val dates: ArrayList<String>
+        try {
+            dates = getDates(5)
+            StoryManager.searchByOutlet(dates,term,outlet,skyList)
+            Timber.i("Search Success")
+        }
+        catch (e: java.lang.Exception) {
+            Timber.i("Search Error : $e.message")
+        }
+    }
+}
