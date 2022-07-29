8000f02fa5db653818caa841e35927b7ba4df13b
Ben Capper <Bencapper19@gmail.com>
2022-07-29 17:48:28 +0100

    Vox Frag/VM/Menu
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/home/Home.kt
+++ app/src/main/java/org/ben/news/ui/home/Home.kt
@@ -188,6 +188,12 @@ class Home : AppCompatActivity() {
         Timber.i("UserId = ${loggedInViewModel.liveFirebaseUser.value!!.uid}")
     }
 
+    fun voxArticles(item: MenuItem) {
+        findNavController(R.id.nav_host_fragment).navigate(R.id.voxFragment)
+        findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawers()
+        Timber.i("UserId = ${loggedInViewModel.liveFirebaseUser.value!!.uid}")
+    }
+
     fun gloArticles(item: MenuItem) {
         findNavController(R.id.nav_host_fragment).navigate(R.id.globalFragment)
         findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawers()
