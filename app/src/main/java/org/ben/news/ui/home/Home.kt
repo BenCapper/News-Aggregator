c71fc0f32c2823291c7c04f5f245aa5d86384fe1
Ben Capper <Bencapper19@gmail.com>
2022-07-30 15:52:16 +0100

    Uk Frag/VM/Menu
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/home/Home.kt
+++ app/src/main/java/org/ben/news/ui/home/Home.kt
@@ -206,6 +206,12 @@ class Home : AppCompatActivity() {
         Timber.i("UserId = ${loggedInViewModel.liveFirebaseUser.value!!.uid}")
     }
 
+    fun ukArticles(item: MenuItem) {
+        findNavController(R.id.nav_host_fragment).navigate(R.id.ukFragment)
+        findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawers()
+        Timber.i("UserId = ${loggedInViewModel.liveFirebaseUser.value!!.uid}")
+    }
+
     fun voxArticles(item: MenuItem) {
         findNavController(R.id.nav_host_fragment).navigate(R.id.voxFragment)
         findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawers()
