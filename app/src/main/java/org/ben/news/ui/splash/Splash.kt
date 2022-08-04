981e9a62d9d40b194a0cfcde958f0cedf3002a4f
Ben Capper <Bencapper19@gmail.com>
2022-08-04 16:14:14 +0100

    Splash Screen Themes
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/splash/Splash.kt
+++ app/src/main/java/org/ben/news/ui/splash/Splash.kt
@@ -1,19 +1,31 @@
 package org.ben.news.ui.splash
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import org.ben.news.R
 import org.ben.news.ui.auth.Login
+import pl.droidsonroids.gif.GifImageView
 
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        val imageView = findViewById<GifImageView>(R.id.splashy)
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                Glide.with(this).load(R.drawable.animb).into(imageView)
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                Glide.with(this).load(R.drawable.animw).into(imageView)
+            }
+        }
         supportActionBar?.hide()
 
         val handler = Handler(Looper.getMainLooper())
