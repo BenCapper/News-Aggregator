3703b59f4ea8cbca2110156e14f4b7f1d8938784
Ben Capper <Bencapper19@gmail.com>
2022-08-05 11:52:55 +0100

    Swipe Refresh - Blaze
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/blaze/BlazeFragment.kt
+++ app/src/main/java/org/ben/news/ui/blaze/BlazeFragment.kt
@@ -63,11 +63,24 @@ class BlazeFragment : Fragment(), StoryListener {
         blazeViewModel.observableBlazeList.observe(viewLifecycleOwner) { story ->
             story?.let {
                 render(story as ArrayList<StoryModel>)
+                checkSwipeRefresh()
             }
             hideLoader(loader)
         }
-
+        setSwipeRefresh()
         return root
+    }
+
+    private fun setSwipeRefresh() {
+        fragBinding.swipe.setOnRefreshListener {
+            fragBinding.swipe.isRefreshing = true
+            blazeViewModel.load()
+        }
+    }
+
+    private fun checkSwipeRefresh() {
+        if (fragBinding.swipe.isRefreshing)
+            fragBinding.swipe.isRefreshing = false
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
