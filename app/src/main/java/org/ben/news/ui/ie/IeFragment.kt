f4311d6fe5fd9d85265e84d93023237c83d8eae1
Ben Capper <Bencapper19@gmail.com>
2022-08-08 21:21:48 +0100

    Better FAB Scroll - IE
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/ie/IeFragment.kt
+++ app/src/main/java/org/ben/news/ui/ie/IeFragment.kt
@@ -12,6 +12,7 @@ import android.widget.ImageView
 import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -60,11 +61,30 @@ class IeFragment : Fragment(), StoryListener {
         activity?.findViewById<ImageView>(R.id.toolimg)?.setImageResource(R.drawable.irish)
         activity?.findViewById<BottomNavigationView>(R.id.bottom_nav)?.visibility = View.VISIBLE
         MobileAds.initialize(this.context!!) {}
-        val bot = activity?.findViewById<BottomNavigationView>(R.id.bottom_nav)
         val fab = activity?.findViewById<FloatingActionButton>(R.id.fab)
+        val bot = activity?.findViewById<BottomNavigationView>(R.id.bottom_nav)
+        fab?.visibility = View.INVISIBLE
+        fragBinding.recyclerViewIe.addOnScrollListener (object : RecyclerView.OnScrollListener(){
+            var y = 0
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                y = dy
+                super.onScrolled(recyclerView, dx, dy)
+
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (y > 0){
+                    fab!!.visibility = View.VISIBLE
+                }
+                else {
+                    fab!!.visibility = View.INVISIBLE
+                }
+            }
+        })
         fab!!.setOnClickListener {
             fragBinding.recyclerViewIe.smoothScrollToPosition(0)
-            bot?.visibility = View.VISIBLE
+            bot!!.visibility = View.VISIBLE
         }
 
         ieViewModel.observableIeList.observe(viewLifecycleOwner) { story ->
