ecd99d4b91a90b95c24c55665551b4f157c5003a
Ben Capper <Bencapper19@gmail.com>
2022-08-12 11:17:13 +0100

    Cbs Script Comments
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- scripts/cbs.py
+++ scripts/cbs.py
@@ -7,7 +7,8 @@ from firebase_admin import storage
  
 from utils.utilities import (formatDate, imgFolder, imgTitleFormat, initialise,
                             logFolder, pageSoup, pushToDB, titleFormat, similar,getHour)
- 
+
+# Set Global Variables
 ref_list = []
 log_file_path = "/home/bencapper/src/News-Aggregator/scripts/log/cbsdone.log"
 log_folder_path = "/home/bencapper/src/News-Aggregator/scripts/log/"
@@ -19,9 +20,12 @@ img_path = "/home/bencapper/src/News/Cbs"
 storage_path = "https://firebasestorage.googleapis.com/v0/b/news-a3e22.appspot.com/o"
 db_path = "stories"
 outlet = "www.CbsNews.com"
- 
+
+# Set Local Folders
 logFolder(log_folder_path)
- 
+imgFolder(img_path)
+
+# Read from Existing Log
 if os.path.exists(log_file_path):
    open_temp = open(log_file_path, "r")
    read_temp = open_temp.read()
@@ -29,29 +33,58 @@ if os.path.exists(log_file_path):
 else:
    os.mknod(log_file_path)
  
-imgFolder(img_path)
+# Initialize Firebase
 initialise(json_path, db_url, bucket)
- 
- 
+
+# Order Based on Current Hour
+# Reversed in Android Studio
+# to Make Sure The Most Recent
+# Articles are Shown First
+order = getHour() 
+
+# Gather News Page HTML
+# Find the List of Articles
 soup = pageSoup(page_url)
 articles = soup.find_all("article")
-order = getHour()
+
+# Cycle through List just Gathered
+# And Save to DB or Pass due to Lack
+# of Information Available
 for article in articles:
+
+    # Catch all for a Litany of Possible Errors
     try:
+
+        # Unwanted Article Type
         if "item--type-video" in str(article):
             pass
+
         else:
+
+            # Gather Title from Article
+            # Format with  a Utils Function
+            # Format Title to get Image Title
             title = article.select("h4")
             title = str(title).split('">')[1].split('</')[0].lstrip().rstrip()
             title = titleFormat(title)
             img_title = imgTitleFormat(title)
+
+            # Select Image Tag from Article HTML
+            # Finalize Image Link
             img_src = article.select("img")
             img_src = str(img_src).split(' data-srcset="')[1].split(' ')[0]
+
+            # Get Link to the article
             link = article.select("a")
             link = str(link).split('href="')[1].split('" ')[0]
 
+            # Get Full Article Page
             full_page = requests.get(link).content
             articleSoup = BeautifulSoup(full_page, features="lxml")
+
+            
+            # Gather Date from Article
+            # Format with a Utils Function
             date = articleSoup.select("time")
             if "Updated on:" in str(date):
                 dates = list()
@@ -68,18 +101,33 @@ for article in articles:
                 dates.append(date[2])
                 date = formatDate(dates)
 
+            # Initialize Storage Variables
             bucket = storage.bucket()
             token = ""
+            
+            # Check if an Article which is 80%+
+            # Similar to any Other in the Log
+            # Similar function in Utils
             check = False
             for ref in ref_list:
                similarity = similar(ref,title)
                if similarity > .8:
                   check = True
                   break
+
+            # Only Continue if the Title is not
+            # Already in the Log and is not too
+            # Similar to Another
             if title not in ref_list and check is False:
+
+                # Add the Title to the List
+                # of Titles Already in the Log
                 ref_list.append(title)
                 open_temp = open(log_file_path, "a")
-                # use images from folder to upload to storage
+
+                # Get Image Data using Requests
+                # Create the Image Locally
+                # Upload image to Storage
                 with open(f"{img_path}/{img_title}", "wb") as img:
                     img.write(requests.get(img_src).content)
                     blob = bucket.blob(f"Cbs/{img_title}")
@@ -87,14 +135,28 @@ for article in articles:
                     metadata = {"firebaseStorageDownloadTokens": token}
                     blob.upload_from_filename(f"{img_path}/{img_title}")
 
+                # Get Link to the Stored Image
                 storage_link = f"https://firebasestorage.googleapis.com/v0/b/news-a3e22.appspot.com/o/Cbs%2F{img_title}?alt=media&token={token}"
 
+                # Push the Gathered Data to DB
+                # Using Utils method
                 pushToDB(
                     db_path, title, date, img_src, img_title, link, outlet, storage_link, order
                 )
+
+                # Write Title to Local Log File
                 open_temp.write(str(title) + "\n")
+
+                # Return Confirmation of New DB Entry 
                 print("CBS Article Added to DB")
+
+            # Title was Already in the Log List
+            # or too Similar to another
             else:
                 print("CBS Article Already in DB")
+    
+    # One of Many Possible Things
+    # Went Wrong - 
+    # Too Much of This is an Issue
     except:
         ("CBS Article Error")
