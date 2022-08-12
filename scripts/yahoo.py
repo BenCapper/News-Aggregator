e55c1d57a39b6539383c519c4695736b50e11192
Ben Capper <Bencapper19@gmail.com>
2022-08-12 14:38:24 +0100

    Yahoo Script Comments
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- scripts/yahoo.py
+++ scripts/yahoo.py
@@ -7,7 +7,7 @@ from firebase_admin import storage
 from utils.utilities import (formatDate, imgFolder, initialise,
                             logFolder, pageSoup, pushToDB, titleFormat, similar,getHour)
  
-
+# Set Global Variables
 ref_list = []
 log_file_path = "/home/bencapper/src/News-Aggregator/scripts/log/yahoodone.log"
 log_folder_path = "/home/bencapper/src/News-Aggregator/scripts/log/"
@@ -19,34 +19,57 @@ img_path = "/home/bencapper/src/News/Yahoo"
 storage_path = "https://firebasestorage.googleapis.com/v0/b/news-a3e22.appspot.com/o"
 db_path = "stories"
 outlet = "news.Yahoo.com"
- 
+
+# Set Local Folders
 logFolder(log_folder_path)
- 
+imgFolder(img_path)
+
+# Read from Existing Log
 if os.path.exists(log_file_path):
    open_temp = open(log_file_path, "r")
    read_temp = open_temp.read()
    ref_list = read_temp.splitlines()
 else:
    os.mknod(log_file_path)
- 
-imgFolder(img_path)
+
+# Initialize Firebase
 initialise(json_path, db_url, bucket)
+
+# Order Based on Current Hour
+# Reversed in Android Studio
+# to Make Sure The Most Recent
+# Articles are Shown First
+order = getHour()
  
- 
+# Gather News Page HTML
+# Find the Div Containing
+# Targeted Article Links
 soup = pageSoup(page_url)
 articles = soup.find_all("div", "Cf")
-order = getHour()
+
+# Cycle through List just Gathered
+# And Save to DB or Pass due to Lack
+# of Information Available
 for article in articles:
+
+   # Catch all for a Litany of Possible Errors
    try:
+
+      # Get Article Link
       a = article.select("a")
       link = str(a).split('href="')[1].split('">')[0]
       link = f"{page_url}/{link}"
+
+      # Get Full Article Page
       full_page = requests.get(link).content
       articleSoup = BeautifulSoup(full_page, features="lxml")
+
+      # Gather Title
       title = articleSoup.select("h1")
       title = str(title).split('">')[1].split('</')[0]
       title = titleFormat(title)
-      img_title = ""
+
+      # Gather / Format Date
       date = list()
       dates = articleSoup.select("time")
       monthDay = str(dates).split('">')[1].split(',')[0].split(' ')
@@ -56,25 +79,47 @@ for article in articles:
       date.append(year)
       date = formatDate(date)
 
+      # No Images
+      img_title = ""
       img_src = ""
+
+      # Check if an Article which is 80%+
+      # Similar to any Other in the Log
+      # Similar function in Utils
       check = False
       for ref in ref_list:
          similarity = similar(ref,title)
          if similarity > .8:
             check = True
             break
+
+      # Only Continue if the Title is not
+      # Already in the Log and is not too
+      # Similar to Another   
       if title not in ref_list and check is False:
+
+         # Add the Title to the List
+         # of Titles Already in the Log
          ref_list.append(title)
          open_temp = open(log_file_path, "a")
+
+         # Get Link to the Stored Image
          storage_link = f"https://firebasestorage.googleapis.com/v0/b/news-a3e22.appspot.com/o/Yahoo%2Fyahoo-news.png?alt=media&token=6ea70d79-fbaf-442d-b952-babb2fb3f6d7"
 
-
+         # Push the Gathered Data to DB
+         # Using Utils method
          pushToDB(
               db_path, title, date, img_src, img_title, link, outlet, storage_link, order
           )
+
+          # Write Title to Local Log File
          open_temp.write(str(title) + "\n")
          print("Yahoo Article Added to the database")
       else:
          print("Yahoo Article Already in the database")
+
+   # One of Many Possible Things
+   # Went Wrong - 
+   # Too Much of This is an Issue
    except:
       print("Yahoo Article Error")
