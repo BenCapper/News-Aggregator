51f38d4eadcc44d6d3af4222c41cd5a5f330d570
Ben Capper <Bencapper19@gmail.com>
2022-08-18 21:06:48 +0100

    Refactor Models
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/models/DoubleStoryModel.kt
+++ app/src/main/java/org/ben/news/models/DoubleStoryModel.kt
@@ -1,7 +1,6 @@
 package org.ben.news.models
 
 import android.os.Parcelable
-import com.google.firebase.database.Exclude
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -17,23 +16,4 @@ data class DoubleStoryModel(var titlehead: String = "",
                             var link1: String = "",
                             var link2: String = "",
                             var order: Int = 0, ) : Parcelable
-{
-    @Exclude
-    fun toMap(): Map<String, Any?> {
-        return mapOf(
-            "titlehead" to titlehead,
-            "title1" to title1,
-            "title2" to title2,
-            "date1" to date1,
-            "date2" to date2,
-            "outlet1" to outlet1,
-            "outlet2" to outlet2,
-            "storage_link1" to storage_link1,
-            "storage_link2" to storage_link2,
-            "link1" to link1,
-            "link2" to link2,
-            "order" to order
-        )
-    }
 
-}
