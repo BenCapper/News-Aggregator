d8cda048993e6ae9dc1845868455bd5ddba32e3f
Ben Capper <Bencapper19@gmail.com>
2022-08-20 13:17:23 +0100

    Show Loader on Date Change Arrows - Breit
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/breitbart/BreitbartFragment.kt
+++ app/src/main/java/org/ben/news/ui/breitbart/BreitbartFragment.kt
@@ -124,10 +124,12 @@ class BreitbartFragment : Fragment(), StoryListener {
                 val datenow = StoryManager.getDate(day)
                 fragBinding.emptydate.text = datenow
                 fragBinding.larrow.setOnClickListener {
-                day += 1
-                breitbartViewModel.load(day)
+                    showLoader(loader,"")
+                    day += 1
+                    breitbartViewModel.load(day)
                 }
                 fragBinding.rarrow.setOnClickListener {
+                    showLoader(loader,"")
                     day -= 1
                     if (day <= 0 ){
                         day = 0
@@ -142,6 +144,7 @@ class BreitbartFragment : Fragment(), StoryListener {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if( item.itemId == R.id.app_bar_right) {
+            showLoader(loader,"")
             day -= 1
             if (day <= 0 ){
                 day = 0
@@ -149,6 +152,7 @@ class BreitbartFragment : Fragment(), StoryListener {
             breitbartViewModel.load(day)
         }
         if( item.itemId == R.id.app_bar_left) {
+            showLoader(loader,"")
             day += 1
             breitbartViewModel.load(day)
         }
