423ab475f4ff60ed0f9f022b12538a49750c576c
Ben Capper <Bencapper19@gmail.com>
2022-08-20 13:16:21 +0100

    Show Loader on Date Change Arrows - Bong
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/bongino/BonginoFragment.kt
+++ app/src/main/java/org/ben/news/ui/bongino/BonginoFragment.kt
@@ -124,10 +124,12 @@ class BonginoFragment : Fragment(), StoryListener {
                 val datenow = StoryManager.getDate(day)
                 fragBinding.emptydate.text = datenow
                 fragBinding.larrow.setOnClickListener {
+                    showLoader(loader,"")
                     day += 1
                     bonginoViewModel.load(day)
                 }
                 fragBinding.rarrow.setOnClickListener {
+                    showLoader(loader,"")
                     day -= 1
                     if (day <= 0 ){
                         day = 0
@@ -142,6 +144,7 @@ class BonginoFragment : Fragment(), StoryListener {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if( item.itemId == R.id.app_bar_right) {
+            showLoader(loader,"")
             day -= 1
             if (day <= 0 ){
                 day = 0
@@ -149,6 +152,7 @@ class BonginoFragment : Fragment(), StoryListener {
             bonginoViewModel.load(day)
         }
         if( item.itemId == R.id.app_bar_left) {
+            showLoader(loader,"")
             day += 1
             bonginoViewModel.load(day)
         }
