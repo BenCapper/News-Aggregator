0d59d1fb8558474aee1c972ef10aed880adf8da3
Ben Capper <Bencapper19@gmail.com>
2022-08-21 15:04:57 +0100

    Loader on Date Right
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/bongino/BonginoFragment.kt
+++ app/src/main/java/org/ben/news/ui/bongino/BonginoFragment.kt
@@ -144,6 +144,7 @@ class BonginoFragment : Fragment(), StoryListener {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if( item.itemId == R.id.app_bar_right) {
+            showLoader(loader,"")
             day -= 1
             if (day <= 0 ){
                 day = 0
