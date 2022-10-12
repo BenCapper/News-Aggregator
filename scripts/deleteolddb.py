67e3d9ff319fc048b547ebbdb5f560db5c40124f
Ben Capper <Bencapper19@gmail.com>
2022-10-12 10:27:41 +0100

    Delete old data from DB
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

---
+++ scripts/deleteolddb.py
@@ -0,0 +1,16 @@
+from utils.utilities import (initialise, cutOffDate)
+from firebase_admin import db
+
+
+cutdate = cutOffDate()
+print(cutdate)
+json_path = "/home/bencapper/src/News-Aggregator/scripts/news.json"
+image_path = "/home/bencapper/src/News/"
+db_url = "https://news-a3e22-default-rtdb.firebaseio.com/"
+bucket = "news-a3e22.appspot.com"
+
+
+initialise(json_path, db_url, bucket)
+
+ref = db.reference(f"stories/{cutOffDate}")
+ref.delete()
