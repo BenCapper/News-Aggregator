52029c688dc66ed470d94be12c98cf3e50537731
Ben Capper <bencapper19@gmail.com>
2023-01-09 14:58:24 +0000

    Guardian Options Deprecation Update
    Signed-off-by: Ben Capper <bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/guardian/GuardFragment.kt
+++ app/src/main/java/org/ben/news/ui/guardian/GuardFragment.kt
@@ -11,7 +11,10 @@ import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import android.widget.ImageView
 import android.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -37,15 +40,16 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 
-class GuardFragment : Fragment(), StoryListener {
+class GuardFragment : Fragment(), StoryListener, MenuProvider {
 
     companion object {
         fun newInstance() = GuardFragment()
     }
+
     private var _fragBinding: FragmentGuardBinding? = null
     private val fragBinding get() = _fragBinding!!
-    lateinit var loader : AlertDialog
-    private val loggedInViewModel : LoggedInViewModel by activityViewModels()
+    lateinit var loader: AlertDialog
+    private val loggedInViewModel: LoggedInViewModel by activityViewModels()
     private val guardViewModel: GuardViewModel by activityViewModels()
     var state: Parcelable? = null
     var day = 0
@@ -63,7 +67,7 @@ class GuardFragment : Fragment(), StoryListener {
         savedInstanceState: Bundle?
     ): View {
         loader = createLoader(requireActivity())
-        showLoader(loader,"")
+        showLoader(loader, "")
         _fragBinding = FragmentGuardBinding.inflate(inflater, container, false)
         val root = fragBinding.root
 
@@ -73,7 +77,7 @@ class GuardFragment : Fragment(), StoryListener {
         val fab = activity?.findViewById<FloatingActionButton>(R.id.fab)
         val bot = activity?.findViewById<BottomNavigationView>(R.id.bottom_nav)
         fab?.visibility = View.INVISIBLE
-        fragBinding.recyclerViewGua.addOnScrollListener (object : RecyclerView.OnScrollListener(){
+        fragBinding.recyclerViewGua.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             var y = 0
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 y = dy
@@ -83,10 +87,9 @@ class GuardFragment : Fragment(), StoryListener {
 
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
-                if (y > 0){
+                if (y > 0) {
                     fab!!.visibility = View.VISIBLE
-                }
-                else {
+                } else {
                     fab!!.visibility = View.INVISIBLE
                 }
             }
@@ -102,38 +105,46 @@ class GuardFragment : Fragment(), StoryListener {
                 checkSwipeRefresh()
             }
             hideLoader(loader)
-            if(fragBinding.recyclerViewGua.adapter!!.itemCount == 0 && searching != null){
+            if (fragBinding.recyclerViewGua.adapter!!.itemCount == 0 && searching != null) {
                 val st = ArrayList<StoryModel>()
-                st.add(StoryModel(title="1"))
+                st.add(StoryModel(title = "1"))
                 fragBinding.recyclerViewGua.adapter = EmptyAdapter(st, this)
                 state?.let { fragBinding.recyclerViewGua.layoutManager?.onRestoreInstanceState(it) }
-            }
-            else if(fragBinding.recyclerViewGua.adapter!!.itemCount == 0){
+            } else if (fragBinding.recyclerViewGua.adapter!!.itemCount == 0) {
                 fragBinding.creepy.visibility = View.VISIBLE
             }
-            if (fragBinding.recyclerViewGua.adapter!!.itemCount > 0)
+            if (fragBinding.recyclerViewGua.adapter!!.itemCount > 0) {
                 fragBinding.creepy.visibility = View.INVISIBLE
-                Glide.with(this).load(R.drawable.bidenlost).into(fragBinding.imageView2)
-                val datenow = StoryManager.getDate(day)
-                fragBinding.emptydate.text = datenow
-                fragBinding.larrow.setOnClickListener {
-                    if (day < 14) {
-                        showLoader(loader, "")
-                        day += 1
-                        guardViewModel.load(day)
-                    }
+            }
+            Glide.with(this).load(R.drawable.bidenlost).into(fragBinding.imageView2)
+            val datenow = StoryManager.getDate(day)
+            fragBinding.emptydate.text = datenow
+            fragBinding.larrow.setOnClickListener {
+                if (day < 14) {
+                    showLoader(loader, "")
+                    day += 1
+                    guardViewModel.load(day)
                 }
-                fragBinding.rarrow.setOnClickListener {
-                    showLoader(loader,"")
+            }
+            fragBinding.rarrow.setOnClickListener {
+                if (day != 0) {
+                    showLoader(loader, "")
                     day -= 1
-                    if (day <= 0 ){
+                    if (day <= 0) {
                         day = 0
                     }
                     guardViewModel.load(day)
                 }
+            }
         }
         setSwipeRefresh()
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     private fun setSwipeRefresh() {
@@ -149,80 +160,9 @@ class GuardFragment : Fragment(), StoryListener {
             fragBinding.swipe.isRefreshing = false
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_all, menu)
-        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_YES -> {
-                menu.findItem(R.id.app_bar_right).iconTintList = null
-                menu.findItem(R.id.app_bar_left).iconTintList = null
-            }
-        }
-        /* Finding the search bar in the menu and setting it to the search view. */
-        val item = menu.findItem(R.id.app_bar_search)
-        val searchView = item.actionView as SearchView
-
-
-        /* This is the code that is executed when the search bar is used. It searches the database for
-        the building that the user is searching for. */
-        searchView.setOnQueryTextListener(object :  SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if (newText != null) {
-                    searching = newText
-                    guardViewModel.search(
-                        day,
-                        newText
-                    )
-                }
-                else{
-                    searching = newText
-                    guardViewModel.load(day)
-                }
-                if (newText == "") {
-                    searching = newText
-                    guardViewModel.load(day)
-                }
-
-                return true
-            }
-        })
-        searchView.setOnCloseListener {
-            searching = null
-            guardViewModel.load(day)
-            false
-        }
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-
     private fun render(storyList: ArrayList<StoryModel>) {
         fragBinding.recyclerViewGua.adapter = StoryAdapter(storyList, this)
         state?.let { fragBinding.recyclerViewGua.layoutManager?.onRestoreInstanceState(it) }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if( item.itemId == R.id.app_bar_right) {
-            if(day != 0) {
-                showLoader(loader, "")
-                day -= 1
-                if (day <= 0) {
-                    day = 0
-                }
-                guardViewModel.load(day)
-            }
-        }
-        if( item.itemId == R.id.app_bar_left) {
-            if (day < 14) {
-                showLoader(loader, "")
-                day += 1
-                guardViewModel.load(day)
-            }
-        }
-        return super.onOptionsItemSelected(item)
-
     }
 
     override fun onResume() {
@@ -236,7 +176,7 @@ class GuardFragment : Fragment(), StoryListener {
     }
 
     override fun onStoryClick(story: StoryModel) {
-        StoryManager.createLiked(loggedInViewModel.liveFirebaseUser.value!!.uid,"history", story)
+        StoryManager.createLiked(loggedInViewModel.liveFirebaseUser.value!!.uid, "history", story)
         val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(story.link))
         state = fragBinding.recyclerViewGua.layoutManager?.onSaveInstanceState()
         startActivity(intent)
@@ -245,11 +185,17 @@ class GuardFragment : Fragment(), StoryListener {
     override fun onLike(story: StoryModel) {
         activity?.alertDialog {
             messageResource = R.string.save_art
-            okButton { StoryManager.createLiked(loggedInViewModel.liveFirebaseUser.value!!.uid,"likes", story)
+            okButton {
+                StoryManager.createLiked(
+                    loggedInViewModel.liveFirebaseUser.value!!.uid,
+                    "likes",
+                    story
+                )
                 val params = fragBinding.root.layoutParams as FrameLayout.LayoutParams
                 params.gravity = Gravity.CENTER_HORIZONTAL
-                view?.snack(R.string.saved_article)}
-            cancelButton{ view?.snack(R.string.save_can)}
+                view?.snack(R.string.saved_article)
+            }
+            cancelButton { view?.snack(R.string.save_can) }
         }?.onShow {
             positiveButton.textColorResource = R.color.black
             negativeButton.textColorResource = splitties.material.colors.R.color.grey_500
@@ -270,9 +216,77 @@ class GuardFragment : Fragment(), StoryListener {
         state = fragBinding.recyclerViewGua.layoutManager?.onSaveInstanceState()
         startActivity(shareIntent)
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _fragBinding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_all, menu)
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                menu.findItem(R.id.app_bar_right).iconTintList = null
+                menu.findItem(R.id.app_bar_left).iconTintList = null
+            }
+        }
+        /* Finding the search bar in the menu and setting it to the search view. */
+        val item = menu.findItem(R.id.app_bar_search)
+        val searchView = item.actionView as SearchView
+
+
+        /* This is the code that is executed when the search bar is used. It searches the database for
+        the building that the user is searching for. */
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    searching = newText
+                    guardViewModel.search(
+                        day,
+                        newText
+                    )
+                } else {
+                    searching = newText
+                    guardViewModel.load(day)
+                }
+                if (newText == "") {
+                    searching = newText
+                    guardViewModel.load(day)
+                }
+
+                return true
+            }
+        })
+        searchView.setOnCloseListener {
+            searching = null
+            guardViewModel.load(day)
+            false
+        }
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.app_bar_right) {
+            if (day != 0) {
+                showLoader(loader, "")
+                day -= 1
+                if (day <= 0) {
+                    day = 0
+                }
+                guardViewModel.load(day)
+            }
+        }
+        if (item.itemId == R.id.app_bar_left) {
+            if (day < 14) {
+                showLoader(loader, "")
+                day += 1
+                guardViewModel.load(day)
+            }
+        }
+        return false
     }
 
 }
