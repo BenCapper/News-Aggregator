dd46973bc47e9cff300bd74480c3a9537366c212
Ben Capper <bencapper19@gmail.com>
2023-01-09 13:59:45 +0000

    Breitbart Options Deprecation Update
    Signed-off-by: Ben Capper <bencapper19@gmail.com>

--- app/src/main/java/org/ben/news/ui/breitbart/BreitbartFragment.kt
+++ app/src/main/java/org/ben/news/ui/breitbart/BreitbartFragment.kt
@@ -11,7 +11,10 @@ import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import android.widget.ImageView
 import android.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -37,7 +40,7 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 
-class BreitbartFragment : Fragment(), StoryListener {
+class BreitbartFragment : Fragment(), StoryListener, MenuProvider {
 
     companion object {
         fun newInstance() = BreitbartFragment()
@@ -137,26 +140,12 @@ class BreitbartFragment : Fragment(), StoryListener {
         return root
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if( item.itemId == R.id.app_bar_right) {
-            if(day != 0) {
-                showLoader(loader, "")
-                day -= 1
-                if (day <= 0) {
-                    day = 0
-                }
-                breitbartViewModel.load(day)
-            }
-        }
-        if( item.itemId == R.id.app_bar_left) {
-            if (day < 14) {
-                showLoader(loader, "")
-                day += 1
-                breitbartViewModel.load(day)
-            }
-        }
-        return super.onOptionsItemSelected(item)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
+
 
 
     private fun setSwipeRefresh() {
@@ -171,55 +160,6 @@ class BreitbartFragment : Fragment(), StoryListener {
         if (fragBinding.swipe.isRefreshing)
             fragBinding.swipe.isRefreshing = false
     }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_all, menu)
-        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_YES -> {
-                menu.findItem(R.id.app_bar_right).iconTintList = null
-                menu.findItem(R.id.app_bar_left).iconTintList = null
-            }
-        }
-        /* Finding the search bar in the menu and setting it to the search view. */
-        val item = menu.findItem(R.id.app_bar_search)
-        val searchView = item.actionView as SearchView
-
-
-        /* This is the code that is executed when the search bar is used. It searches the database for
-        the building that the user is searching for. */
-        searchView.setOnQueryTextListener(object :  SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if (newText != null) {
-                    searching = newText
-                    breitbartViewModel.search(
-                        day,
-                        newText
-                    )
-                }
-                else{
-                    searching = newText
-                    breitbartViewModel.load(day)
-                }
-                if (newText == "") {
-                    searching = newText
-                    breitbartViewModel.load(day)
-                }
-
-                return true
-            }
-        })
-        searchView.setOnCloseListener {
-            searching = null
-            breitbartViewModel.load(day)
-            false
-        }
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
 
     private fun render(storyList: ArrayList<StoryModel>) {
         fragBinding.recyclerViewBreit.adapter = StoryAdapter(storyList, this)
@@ -274,6 +214,74 @@ class BreitbartFragment : Fragment(), StoryListener {
     override fun onDestroyView() {
         super.onDestroyView()
         _fragBinding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_all, menu)
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                menu.findItem(R.id.app_bar_right).iconTintList = null
+                menu.findItem(R.id.app_bar_left).iconTintList = null
+            }
+        }
+        /* Finding the search bar in the menu and setting it to the search view. */
+        val item = menu.findItem(R.id.app_bar_search)
+        val searchView = item.actionView as SearchView
+
+
+        /* This is the code that is executed when the search bar is used. It searches the database for
+        the building that the user is searching for. */
+        searchView.setOnQueryTextListener(object :  SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    searching = newText
+                    breitbartViewModel.search(
+                        day,
+                        newText
+                    )
+                }
+                else{
+                    searching = newText
+                    breitbartViewModel.load(day)
+                }
+                if (newText == "") {
+                    searching = newText
+                    breitbartViewModel.load(day)
+                }
+
+                return true
+            }
+        })
+        searchView.setOnCloseListener {
+            searching = null
+            breitbartViewModel.load(day)
+            false
+        }
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean {
+        if( item.itemId == R.id.app_bar_right) {
+            if(day != 0) {
+                showLoader(loader, "")
+                day -= 1
+                if (day <= 0) {
+                    day = 0
+                }
+                breitbartViewModel.load(day)
+            }
+        }
+        if( item.itemId == R.id.app_bar_left) {
+            if (day < 14) {
+                showLoader(loader, "")
+                day += 1
+                breitbartViewModel.load(day)
+            }
+        }
+        return false
     }
 
 }
